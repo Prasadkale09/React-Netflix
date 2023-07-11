f8b50bdda21eea9de89f90dee7271ccbc1ac43b1
Prasad_kale <prasadkale2581@gmail.com>
2023-07-11 12:52:23 +0530

    add responsive code

--- src/componant/Imformation.jsx
+++ src/componant/Imformation.jsx
@@ -1,5 +1,6 @@
 const Imformation = [
   {
+    id : 1,
     imgsrc: "https://wallpapercave.com/w/wp6354988.jpg",
     category: "A Netflix Origanal Series",
     title: "Elite",
@@ -7,30 +8,35 @@ const Imformation = [
   },
 
   {
+    id : 2,
     imgsrc: "https://wallpapercave.com/w/wp8974496.jpg",
     category: "A Netflix Origanal Series",
     title: "Asur",
     link: "https://www.jiocinema.com/tv-shows/asur/2/the-dance-of-death/3757231?utm_source=google_web&utm_medium=search_google&utm_campaign=Avod_PM_JioCinema_Web_Search_Asur2_23May23&gclid=Cj0KCQjwho-lBhC_ARIsAMpgMof1t15RhwjhPCS_9h37pBkq22FYQp38lZ3S2FCgD3i0pnjObFDQwvcaApvEEALw_wcB",
   },
   {
+    id : 3,
     imgsrc: "https://wallpapercave.com/w/wp8547883.jpg",
     category: "A Netflix Origanal Series",
     title: "Criminal Justice",
     link: "https://www.imdb.com/title/tt9095260/",
   },
   {
+    id : 4,
     imgsrc: "https://wallpapercave.com/w/wp5135012.jpg",
     category: "A Netflix Origanal Series",
     title: "See",
     link: "https://www.netflix.com/in/title/80100172",
   },
   {
+    id : 5,
     imgsrc: "https://wallpapercave.com/w/wp5179328.jpg",
     category: "A Netflix Origanal Series",
     title: "You",
     link: "https://www.netflix.com/in/title/80100172",
   },
   {
+    id : 6,
     imgsrc: "https://wallpapercave.com/w/wp6903007.jpg",
     category: "A Netflix Origanal Series",
     title: "Flames",
--- src/index.js
+++ src/index.js
@@ -4,19 +4,18 @@ import Card from "./componant/Card";
 import Imformation from "./componant/Imformation";
 import "./index.css";
 
-
-
 ReactDOM.render(
   <>
-  
-  <div id="Heading">
-    <h1 className="Heading">Top 5 Netflix Web Series</h1>
+    <div id="Heading">
+      <h1 className="Heading">Top 5 Netflix Web Series</h1>
     </div>
     <div className="All">
-    {/* fat Arrow function */}
-      {Imformation.map((val,index)=>{
+      
+      {/* fat Arrow function */}
+      {Imformation.map((val, index) => {
         return (
           <Card
+            key={val.id}
             imgsrc={val.imgsrc}
             category={val.category}
             title={val.title}
